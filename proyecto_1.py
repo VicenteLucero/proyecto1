cb572fc110521bc76ca806d99f35765124d80e45
Vicente Lucero <31738361+VicenteLucero@users.noreply.github.com>
2020-08-09 21:58:56 -0400

    14 min

--- proyecto_1.py
+++ proyecto_1.py
@@ -1,7 +1,7 @@
 import sys
 import re
 import threading
-
+from timeit import default_timer as timer
 
 def contar_vocales(palabra):
     vocals = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
@@ -58,13 +58,14 @@ def sequences(ind, word, data, comands):
                 frase = frase + ' ' + word2
                 continue
         return
-        # print(frase)
+    # print(frase)
     frases.append(frase)
 
 vocales = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
-comands = open('comands.txt', 'r').read().split('\n')
 # print(comands)
-text_name = "enwik8.txt"
+text_name = input("text: ")
+start = timer()
+comands = open('comands.txt', 'r').read().split('\n')
 text_file = open(text_name, encoding="utf8")
 text = text_file.read()
 text = re.sub(r'[^a-zA-Z \n]', '', text)
@@ -72,53 +73,38 @@ text = re.sub(r'\n', ' ', text)
 data = re.findall(r'[a-zA-Z]+\b', text)
 frases = []
 for c in comands:
+        temp_data = data
         c = c.split()
         print(c)
         first = c.pop(0)
         # print(first)
-        for i in range(0, len(data)):
-            word = data[i]
-            if first[0] == 'a':
-                if len(word) == int(first[1:]):
-                    sequences(i, word, data, c)
-                    continue
-            elif first[0] == 'b':
-                if int(first[1:]) == contar_vocales(word):
-                    sequences(i, word, data, c)
-                    continue
-            elif first == 'c':
-                if contar_vocales(word) > contar_conso(word):
-                    sequences(i, word, data, c)
-                    continue
-            elif first == 'c-':
-                if contar_vocales(word) <= contar_conso(word):
-                    sequences(i, word, data, c)
-                    continue
-            elif first == 'd':
-                if word[0] in vocales:
-                    sequences(i, word, data, c)
-                    continue
-            elif first == 'd-':
-                if word[0] not in vocales:
-                    sequences(i, word, data, c)
-                    continue
-            elif first == 'e':
-                if word[-1] in vocales:
-                    sequences(i, word, data, c)
-                    continue
-            elif first == 'e-':
-                if word[-1] not in vocales:
-                    sequences(i, word, data, c)
-                    continue
+        if first[0] == 'a':
+            temp_data = [w for w in range(0, len(data)) if len(data[w]) == int(first[1:])]
+        elif first[0] == 'b':
+            temp_data = [w for w in range(0, len(data)) if contar_vocales(data[w]) == int(first[1:])]
+        elif first == 'c':
+            temp_data = [w for w in range(0, len(data)) if contar_vocales(data[w]) > contar_conso(data[w])]
+        elif first == 'c-':
+            temp_data = [w for w in range(0, len(data)) if contar_vocales(data[w]) <= contar_conso(data[w])]
+        elif first == 'd':
+            temp_data = [w for w in range(0, len(data)) if data[w][0] in vocales]
+        elif first == 'd-':
+            temp_data = [w for w in range(0, len(data)) if data[w][0] not in vocales]
+        elif first == 'e':
+            temp_data = [w for w in range(0, len(data)) if data[w][-1] in vocales]
+        elif first == 'e-':
+            temp_data = [w for w in range(0, len(data)) if data[w][-1] not in vocales]
+        # print(temp_data)
+        for i in temp_data:
+            sequences(i, data[i], data, c)
             # print(word)
             # thread = threading.Thread(target=sequences, args=(i, word, data, comands))
             # thread.start()
-        #print(len(frases))
-        # comands = []
-        #print()
 print(len(frases))
 output = open('salida.txt', 'w')
 output.write(str(len(frases))+'\n')
 for f in frases:
     output.write(f+'\n')
 output.close()
+end = timer()
+# print(end - start)
